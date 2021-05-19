acea7a9709b8f3664b6beb405e666c10f0f5ac09
Aless <arodriguezshule@frba.utn.edu.ar>
2021-05-18 23:28:36 -0300

    Agregue Main.c y TP1.c

---
+++ Main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "TP1.c"
+
+int main(){
+
+}
---
+++ TP1.c
@@ -0,0 +1,9 @@
+
+// a) Calcular y retornar la longitud de una cadena dada.
+// b) Convertir una cadena de dígitos en su equivalente numérico.
+// c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres
+// en mayúscula.
+// d) Eliminar de una cadena dada todas las ocurrencias de un carácter dado.
+// e) Concatenar al final de la primer cadena dada una segunda cadena dada .
+// f) Modificar la cadena dada con la inserción de un carácter dado en una posición
+// determinada.
