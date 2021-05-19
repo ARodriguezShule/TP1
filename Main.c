f56857def57924ff4f0f97ca393a2a1e53d4be82
Aless <arodriguezshule@frba.utn.edu.ar>
2021-05-19 00:40:33 -0300

    Agregue punto a , punto b y una funcione extra

--- Main.c
+++ Main.c
@@ -2,5 +2,6 @@
 #include "TP1.c"
 
 int main(){
-
-}
+    printf("Ejercicios Trabajo practico 1:\n");
+    printf("largo de 'demostracion' :\t%d", largo("demostracion"));
+    }
--- TP1.c
+++ TP1.c
@@ -1,9 +1,14 @@
 
 // a) Calcular y retornar la longitud de una cadena dada.
+int largo (char s[]){
+    int contador = 0;
+    while(s[contador] != '\0'){
+        contador ++;
+    }
+    return contador;
+}
 // b) Convertir una cadena de dígitos en su equivalente numérico.
-// c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres
-// en mayúscula.
+// c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula.
 // d) Eliminar de una cadena dada todas las ocurrencias de un carácter dado.
 // e) Concatenar al final de la primer cadena dada una segunda cadena dada .
-// f) Modificar la cadena dada con la inserción de un carácter dado en una posición
-// determinada.
+// f) Modificar la cadena dada con la inserción de un carácter dado en una posición determinada.
