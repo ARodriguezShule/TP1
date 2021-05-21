3c197499f5d8135a6941c9584f208eefba14755f
Aless <arodriguezshule@frba.utn.edu.ar>
2021-05-21 15:31:40 -0300

    agregué el punto b (ahora sí) y el punto c

--- Main.c
+++ Main.c
@@ -3,5 +3,6 @@
 
 int main(){
     printf("Ejercicios Trabajo practico 1:\n");
-    printf("largo de 'demostracion' :\t%d", largo("demostracion"));
+    printf("largo de 'demostracion' :\t%d\n", largo("demostracion"));
+    printf("valor numerico de '23651' :\t%d\n", cadenaANumero("23651"));
     }
--- TP1.c
+++ TP1.c
@@ -1,4 +1,14 @@
 
+//Funciones auxiliares
+int  potencia (int a, int b){
+    int sumador = 1;
+    while(b >= 0){
+        sumador *= a;
+        b--;
+    }
+    return sumador;
+}
+
 // a) Calcular y retornar la longitud de una cadena dada.
 int largo (char s[]){
     int contador = 0;
@@ -8,7 +18,25 @@ int largo (char s[]){
     return contador;
 }
 // b) Convertir una cadena de dígitos en su equivalente numérico.
+long cadenaANumero(char c[]){
+    int sumador = 0, largoCadena = largo(c);
+    for(int i = 0;i<largoCadena; i++ ){
+        sumador += (c[i]-48) * potencia(10,largoCadena-i-2); //el 48 es una manera de traducir de ASCII a numero, se ve horrible pero funciona ¯\_(ツ)_/¯
+    }
+    return sumador;
+}
 // c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula.
+char* upperCase ( char s[]){
+    for(unsigned char* c = s ;*c!=0;c++ ){
+        if(*c >= 97 && *c < 122){
+            *c -= 32;
+        }
+        if(*c >=224 & *c < 255){ //no parece funcionar con el ascii extendido, creo que es un tema de la consola.
+            *c -= 36;
+        }
+    }
+    return s;
+}
 // d) Eliminar de una cadena dada todas las ocurrencias de un carácter dado.
 // e) Concatenar al final de la primer cadena dada una segunda cadena dada .
 // f) Modificar la cadena dada con la inserción de un carácter dado en una posición determinada.
