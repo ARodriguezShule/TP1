dc1068a01b1a8b9b72741e3a17a967eea831b7f3
Aless <arodriguezshule@frba.utn.edu.ar>
2021-05-24 12:04:53 -0300

    uní largo() y longitud() eran lo mismo

--- TP1.c
+++ TP1.c
@@ -3,7 +3,7 @@
 int potencia(int a, int b)
 {
     int sumador = 1;
-    while (b >= 0)
+    while (b > 0)
     {
         sumador *= a;
         b--;
@@ -12,37 +12,32 @@ int potencia(int a, int b)
 }
 
 // a) Calcular y retornar la longitud de una cadena dada.
-int largo(char s[])
+int longitud(char cadena[])
 {
-    int contador = 0;
-    while (s[contador] != '\0')
-    {
-        contador++;
-    }
-    return contador;
+    int i;
+
+    for (i = 0; cadena[i] != '\0'; i++)
+        ;
+    return i;
 }
 // b) Convertir una cadena de dígitos en su equivalente numérico.
 long cadenaANumero(char c[])
 {
-    int sumador = 0, largoCadena = largo(c);
+    long sumador = 0, largoCadena = longitud(c);
     for (int i = 0; i < largoCadena; i++)
     {
-        sumador += (c[i] - 48) * potencia(10, largoCadena - i - 2); //el 48 es una manera de traducir de ASCII a numero, se ve horrible pero funciona ¯\_(ツ)_/¯
+        sumador += (c[i] - 48) * potencia(10, largoCadena - i - 1); //el 48 es una manera de traducir de ASCII a numero, se ve horrible pero funciona ¯\_(ツ)_/¯
     }
     return sumador;
 }
 // c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula.
-char *upperCase(char s[])
+char *upperCase(unsigned char s[])
 {
-    for (unsigned char *c = s; *c != 0; c++)
+    for (int i = 0; s[i] != 0; i++)
     {
-        if (*c >= 97 && *c < 122)
+        if (s[i] >= 97 && s[i] < 122)
         {
-            *c -= 32;
-        }
-        if (*c >= 224 & *c < 255)
-        { //no parece funcionar con el ascii extendido, creo que es un tema de la consola.
-            *c -= 36;
+            s[i] -= 32;
         }
     }
     return s;
@@ -83,14 +78,6 @@ void eliminar(char *cadena, char *caracteres)
 }
 // e) Concatenar al final de la primer cadena dada una segunda cadena dada .void concatena(char cadena1[], char cadena2[])
 
-int longitud(char cadena[])
-{
-    int i;
-
-    for (i = 0; cadena[i] != '\0'; i++)
-        ;
-    return i;
-}
 void concatena(char cadena1[], char cadena2[])
 {
     int i, j;
